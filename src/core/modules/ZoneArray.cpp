b93f334f68b107d4126c0123e9173e6d9310d759
Georg Zotti <Georg.Zotti@univie.ac.at>
2025-04-23 18:24:36 +0200

    Add star search within a spherical region - Seems buggy, does by far not deliver everything yet!

--- src/core/modules/ZoneArray.cpp
+++ src/core/modules/ZoneArray.cpp
@@ -590,6 +590,38 @@ void SpecialZoneArray<Star>::searchAround(const StelCore* core, int index, const
 }
 
 template<class Star>
+void SpecialZoneArray<Star>::searchWithin(const StelCore* core, int index, const SphericalRegionP region, const double withParallax, const Vec3d diffPos, const bool hipOnly,
+						  QList<StelObjectP > &result)
+{
+	const float dyrs = static_cast<float>(core->getJDE()-STAR_CATALOG_JDEPOCH)/365.25;
+	const SpecialZoneData<Star> *const z = getZones()+index;
+	Vec3d tmp;
+	double RA, DEC, pmra, pmdec, Plx, RadialVel;
+	for (const Star* s=z->getStars();s<z->getStars()+z->size;++s)
+	{
+		s->getFull6DSolution(RA, DEC, Plx, pmra, pmdec, RadialVel, dyrs);
+		StelUtils::spheToRect(RA, DEC, tmp);
+		// s->getJ2000Pos(dyrs, tmp);
+		// in case it is in a binary system
+		s->getBinaryOrbit(core->getJDE(), tmp);
+		s->getPlxEffect(withParallax * Plx, tmp, diffPos);
+		tmp.normalize();
+		if (core->getUseAberration())
+		{
+			const Vec3d vel = core->getAberrationVec(core->getJDE());
+			tmp+=vel;
+			tmp.normalize();
+		}
+		if (region->contains(tmp))
+		{
+			// TODO: do not select stars that are too faint to display
+			result.push_back(s->createStelObject(this,z));
+		}
+	}
+}
+
+
+template<class Star>
 StelObjectP SpecialZoneArray<Star>::searchGaiaID(int index, const StarId source_id, int &matched) const
 {
 	const SpecialZoneData<Star> *const z = getZones()+index;
